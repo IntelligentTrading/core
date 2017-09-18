7377029f9fbb6fdf502b29f7f3ce5e5403176c0a
Marcelo Barbosa <firemanxbr@fedoraproject.org>
2017-09-19 02:23:20 +0700

    first tests inserting data into BigTable

--- bigtable/create_schema.py
+++ bigtable/create_schema.py
@@ -25,11 +25,34 @@ def create_schema(project_id, instance_id, table_id,
         instance = client.instance(instance_id)
 
         table = instance.table(table_id)
-        table.create()
+        #table.create()
 
-        for cols_f in column_family:
-            cols = table.column_family(cols_f)
-            cols.create()
+        #for cols_f in column_family:
+        #    cols = table.column_family(cols_f)
+        #    cols.create()
+
+        row_key = 'poloniex#201703061617'
+        row = table.row(row_key)
+
+        col_id1 = 'BTC_ETH_LAST'
+        col_id2 = 'BTC_ETH_CHANGE'
+        col_id3 = 'BTC_ETH_HIGH'
+        col_id4 = 'BTC_ETH_LOW'
+        col_id5 = 'BTC_ETH_VOLUME'
+
+        value1 = '1'
+        value2 = '2'
+        value3 = '3'
+        value4 = '4'
+        value5 = '5'
+
+        row.set_cell(column_family[0], col_id1, value1.encode('utf-8'))
+        row.set_cell(column_family[0], col_id2, value2.encode('utf-8'))
+        row.set_cell(column_family[0], col_id3, value3.encode('utf-8'))
+        row.set_cell(column_family[0], col_id4, value4.encode('utf-8'))
+        row.set_cell(column_family[0], col_id5, value5.encode('utf-8'))
+
+        row.commit()
 
         return True
     except Exception as error:
