e789d44f959bff04954eb9d0d1e6e64f0475525a
Tom Counsell <contact@tomcounsell.com>
2017-10-12 18:09:42 -0700

    pull latest data from Poloniex and save

---
+++ apps/channel/management/commands/trawl_poloniex.py
@@ -0,0 +1,92 @@
+import logging
+import time
+
+from django.core.management.base import BaseCommand
+from requests import get, RequestException
+
+from apps.channel.models import ExchangeData
+from apps.channel.models.exchange_data import POLONIEX
+from apps.indicator.models import Price, Volume
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Polls data from Poloniex on a regular interval"
+
+    def handle(self, *args, **options):
+        logger.info("Getting ready to trawl Poloniex...")
+
+        poloniex()
+
+        logger.info("All done.")
+
+
+def poloniex():
+    try:
+        req = get('https://poloniex.com/public?command=returnTicker')
+
+        poloniex_data_point = ExchangeData.objects.create(
+            data=req.json(),
+            timestamp=time.time()
+        )
+
+        data = poloniex_data_point.data
+        timestamp = poloniex_data_point.timestamp
+
+        save_prices(data, timestamp)
+        save_volumes(data, timestamp)
+
+    except RequestException:
+        return 'Error to collect data from Poloniex'
+
+
+def save_prices(data, timestamp):
+    try:
+        usdt_btc = data.pop("USDT_BTC")
+        Price.objects.create(
+            source=POLONIEX,
+            coin="BTC",
+            satoshis=int(10 ** 8),
+            usdt=float(usdt_btc['last']),
+            timestamp=timestamp
+        )
+    except KeyError:
+        logger.debug("missing BTC in Poloniex data")
+
+    for currency_pair in data:
+        if currency_pair.split('_')[0] == "BTC":
+            Price.objects.create(
+                source=POLONIEX,
+                coin=currency_pair.split('_')[1],
+                satoshis=int(float(data[currency_pair]['last']) * 10 ** 8),
+                timestamp=timestamp
+            )
+
+    # trigger indicators
+
+
+def save_volumes(data, timestamp):
+    try:
+        usdt_eth = data.pop("USDT_ETH")
+        btc_eth = data.pop("USDT_ETH")
+        Price.objects.create(
+            source=POLONIEX,
+            coin="ETH",
+            satoshis=int(float(btc_eth['last']) * 10 ** 8),
+            wei=int(10 ** 8),
+            usdt=float(usdt_eth['last']),
+            timestamp=timestamp
+        )
+    except KeyError:
+        logger.debug("missing ETH in Poloniex data")
+
+    for currency_pair in data:
+        if currency_pair.split('_')[0] == "BTC":
+            Volume.objects.create(
+                source=POLONIEX,
+                coin=currency_pair.split('_')[1],
+                btc_volume=int(float(data[currency_pair]['volume']) * 10 ** 8)
+            )
+
+    # trigger indicators
