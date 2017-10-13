de3c4c58a2dd58b688e505af2e3e3f3f30edfc46
Tom Counsell <contact@tomcounsell.com>
2017-10-12 20:25:27 -0700

    try mysql db engine for aurora

--- settings/vendor_services_settings.py
+++ settings/vendor_services_settings.py
@@ -26,7 +26,7 @@ if not LOCAL:
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.mysql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
