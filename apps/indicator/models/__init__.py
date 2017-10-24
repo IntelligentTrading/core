50884e58211b85c1264d36c91358fadaf9a24a44
Tom Counsell <contact@tomcounsell.com>
2017-10-24 19:29:46 +0200

    draft SMA model

--- apps/indicator/models/__init__.py
+++ apps/indicator/models/__init__.py
@@ -1,7 +1,9 @@
 from apps.indicator.models.price import Price
 from apps.indicator.models.volume import Volume
+from apps.indicator.models.sma import SMA
 
 __all__ = [
     Price,
     Volume,
+    SMA,
 ]
