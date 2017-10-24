fa34a615d8c1c7668207f7c44a81fdf32fe68a39
Tom Counsell <contact@tomcounsell.com>
2017-10-24 12:10:17 +0200

    new csv api endpoint

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from apps.api.views import price, volume, user
+from apps.api.views import price, volume, user, csv
 
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'^price$', price.Price.as_view(), name='price'),
     url(r'^volume$', volume.Volume.as_view(), name='volume'),
     # url(r'^sma$', views.sma.SMA.as_view(), name='sma'),
+
+    url(r'^csv$', csv.CSV.as_view(), name='csv'),
 
 ]
---
+++ apps/api/views/csv.py
@@ -0,0 +1,21 @@
+import json
+import csv
+from apps.indicator.models import Price
+from django.http import HttpResponse
+from django.views.generic import View
+
+class CSV(View):
+    def dispatch(self, request, *args, **kwargs):
+        return super(CSV, self).dispatch(request, *args, **kwargs)
+
+
+    def get(self, request, *args, **kwargs):
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+
+        writer = csv.writer(response)
+        for price in Price.objects.all():
+            writer.writerow([price.coin, price.satoshis, str(price.timestamp)])
+
+        return response
