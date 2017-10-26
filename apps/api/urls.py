0a6d33e6a56ed5d8a43ad98bd42af1636b6d86c9
Tom Counsell <contact@tomcounsell.com>
2017-10-26 18:34:10 +0200

    users api webhook

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -5,6 +5,7 @@ from apps.api.views import price, volume, user, csv
 urlpatterns = [
 
     url(r'^user$', user.User.as_view(), name='user'),
+    url(r'^users$', user.Users.as_view(), name='users'),
 
     url(r'^price$', price.Price.as_view(), name='price'),
     url(r'^volume$', volume.Volume.as_view(), name='volume'),
--- apps/api/views/user.py
+++ apps/api/views/user.py
@@ -30,22 +30,13 @@ class User(View):
 
             if request.POST.get('risk', 'n/a') in ['low', 'medium', 'high']:
                 risk_string = request.POST['risk']
-                if risk_string == 'low':
-                    user.risk = UserModel.LOW_RISK
-                elif risk_string == 'medium':
-                    user.risk = UserModel.MEDIUM_RISK
-                elif risk_string == 'high':
-                    user.risk = UserModel.HIGH_RISK
+                user.risk = user.get_risk_value(risk_string)
+                assert user.risk == user.get_risk_value()
 
             if request.POST.get('horizon', 'n/a') in ['short', 'medium', 'long']:
                 horizon_string = request.POST['horizon']
-                if horizon_string == 'short':
-                    user.horizon = UserModel.SHORT_HORIZON
-                elif horizon_string == 'medium':
-                    user.horizon = UserModel.MEDIUM_HORIZON
-                elif horizon_string == 'long':
-                    user.horizon = UserModel.LONG_HORIZON
-
+                user.horizon = user.get_horizon_value(horizon_string)
+                assert user.horizon == user.get_horizon_value()
             user.save()
 
             return HttpResponse(json.dumps(user.get_telegram_settings())) # ok
@@ -55,4 +46,32 @@ class User(View):
             return HttpResponse(json.dumps({'error':str(e)}), status=500) # server error
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse(json.dumps({'error':'POST requests only'}))  # ok
+
+        chat_id = request.GET.get('chat_id', "")
+        if not len(chat_id):
+            return HttpResponse(400)  # request error
+
+        user, u_created = UserModel.objects.get_or_create(telegram_chat_id=chat_id)
+        return HttpResponse(json.dumps(user.get_telegram_settings()))  # ok
+
+
+class Users(View):
+    def dispatch(self, request, *args, **kwargs):
+        return super(Users, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        users = UserModel.objects.filter(is_subscribed=True, is_muted=False)
+
+        risk_string = request.GET.get('risk', 'all')
+        assert risk_string in ['low', 'medium', 'high', 'all']
+        if risk_string is not 'all':
+            users = users.filter(risk = UserModel.get_risk_value(UserModel, risk_string))
+
+        horizon_string = request.GET.get('horizon', 'all')
+        assert horizon_string in ['short', 'medium', 'long', 'all']
+        if horizon_string is not 'all':
+            users = users.filter(horizon=UserModel.get_horizon_value(UserModel, horizon_string))
+
+        chat_id_list = list(users.values_list('telegram_chat_id', flat=True))
+
+        return HttpResponse(json.dumps({'chat_ids': chat_id_list}))  # ok
