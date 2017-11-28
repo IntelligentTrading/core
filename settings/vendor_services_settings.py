61a6216f743174470c8c4e499ffaf0153f4091fa
Tom Counsell <contact@tomcounsell.com>
2017-11-28 09:33:44 +0400

    use postgres on stage

--- settings/vendor_services_settings.py
+++ settings/vendor_services_settings.py
@@ -8,14 +8,40 @@ AWS_OPTIONS = {
     'AWS_SECRET_ACCESS_KEY' : os.environ.get('AWS_SECRET_ACCESS_KEY'),
     'AWS_STORAGE_BUCKET_NAME' : os.environ.get('AWS_STORAGE_BUCKET_NAME'),
 }
+
 if PRODUCTION:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+
     BUCKET_NAME = "intelligenttrading-s3-production"
     QUEUE_NAME = "intelligenttrading-sqs-production" # for production bot
     # DELAYED_QUEUE_NAME = "intelligenttrading-delayed-sqs-production"
     BETA_QUEUE_NAME = "intelligenttrading-sqs-beta" # for beta bot
     TEST_QUEUE_NAME = ""
     SNS_NAME = "intelligenttrading-sns-production"
+
 elif STAGE:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+
     BUCKET_NAME = "intelligenttrading-s3-stage"
     QUEUE_NAME = "intelligenttrading-sqs-stage" # for stage bot
     BETA_QUEUE_NAME = "" # intelligenttrading-sqs-stage-beta
@@ -37,14 +63,3 @@ if not LOCAL:
     STATIC_ROOT = STATIC_URL = AWS_STATIC_URL
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
