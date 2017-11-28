4117987f373067d9edf3530fc73dce3cef9ce216
Tom Counsell <contact@tomcounsell.com>
2017-11-28 10:30:06 +0400

    refactor and remove bucket env var

--- settings/vendor_services_settings.py
+++ settings/vendor_services_settings.py
@@ -2,26 +2,7 @@ import os
 from settings import LOCAL, PRODUCTION, STAGE
 
 
-# AWS
-AWS_OPTIONS = {
-    'AWS_ACCESS_KEY_ID' : os.environ.get('AWS_ACCESS_KEY_ID'),
-    'AWS_SECRET_ACCESS_KEY' : os.environ.get('AWS_SECRET_ACCESS_KEY'),
-    'AWS_STORAGE_BUCKET_NAME' : os.environ.get('AWS_STORAGE_BUCKET_NAME'),
-}
-
 if PRODUCTION:
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-
     BUCKET_NAME = "intelligenttrading-s3-production"
     QUEUE_NAME = "intelligenttrading-sqs-production" # for production bot
     # DELAYED_QUEUE_NAME = "intelligenttrading-delayed-sqs-production"
@@ -30,18 +11,6 @@ if PRODUCTION:
     SNS_NAME = "intelligenttrading-sns-production"
 
 elif STAGE:
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-
     BUCKET_NAME = "intelligenttrading-s3-stage"
     QUEUE_NAME = "intelligenttrading-sqs-stage" # for stage bot
     BETA_QUEUE_NAME = "" # intelligenttrading-sqs-stage-beta
@@ -53,13 +22,29 @@ else: # LOCAL
     pass # see local_settings.py
 
 
-HOST_URL = 'http://' + BUCKET_NAME + '.s3.amazonaws.com'
-
-
 if not LOCAL:
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    MEDIA_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
-    AWS_STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+
+    # AWS
+    AWS_OPTIONS = {
+        'AWS_ACCESS_KEY_ID': os.environ.get('AWS_ACCESS_KEY_ID'),
+        'AWS_SECRET_ACCESS_KEY': os.environ.get('AWS_SECRET_ACCESS_KEY'),
+        'AWS_STORAGE_BUCKET_NAME': BUCKET_NAME,
+    }
+
+    HOST_URL = 'http://' + BUCKET_NAME + '.s3.amazonaws.com'
+    MEDIA_URL = 'http://' + BUCKET_NAME + '.s3.amazonaws.com/'
+    AWS_STATIC_URL = 'http://' + BUCKET_NAME + '.s3.amazonaws.com/'
     STATIC_ROOT = STATIC_URL = AWS_STATIC_URL
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql' if PRODUCTION else 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
