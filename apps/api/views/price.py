61f67581963854a84187002242799bf83be32ce4
Alex Boreysha <lexnex@gmail.com>
2018-02-19 15:57:52 +0100

    add simple filter for prices api

--- apps/api/views/price.py
+++ apps/api/views/price.py
@@ -1,36 +1,51 @@
 from rest_framework.generics import ListAPIView
 
-from rest_framework.views import APIView
-
 from apps.api.serializers import PriceSerializer
 from apps.api.permissions import RestAPIPermission
 from apps.api.paginations import StandardResultsSetPagination, OneRecordPagination
 
 from settings import PERIODS_LIST
 
-from apps.indicator.models import PriceResampl
+from apps.indicator.models import PriceResampl, Price
 
+#from django_filters.rest_framework import DjangoFilterBackend
+
+SHORT_PERIOD = PERIODS_LIST[0] # PERIODS_LIST = [60, 240, 1440] in minutes
 
 class PricesListAPIView(ListAPIView):
-    short_period = PERIODS_LIST[0] # 1min in secs
+    """
+    Show List of Prices from PriceResampl model for short resampl period - 60min.
+    """ 
     queryset = PriceResampl.objects.exclude(midpoint_price__isnull=True # filter empty records
-    ).filter(resample_period=short_period).order_by('-timestamp')
+    ).filter(resample_period=SHORT_PERIOD).order_by('-timestamp')
     
     permission_classes = (RestAPIPermission, )
     pagination_class = StandardResultsSetPagination
     serializer_class = PriceSerializer
 
+    filter_fields = ('source', 'transaction_currency', 'counter_currency')
+
 class PriceListAPIView(ListAPIView):
+    """
+    Show last price from PriceResampl model for short resampl period - 60min.
+    Default counter_currency is BTC. For BTC itself counter_currency is USDT.
+    """
+    
     permission_classes = (RestAPIPermission, )
     serializer_class = PriceSerializer
     pagination_class = OneRecordPagination
 
     model = serializer_class.Meta.model
+
     def get_queryset(self):
-        short_period = PERIODS_LIST[0] # PERIODS_LIST = [60, 240, 1440] in minutes
         transaction_currency = self.kwargs['transaction_currency']
-        # midpoint_price must not be empty, short resample period - 60min
+        if transaction_currency == 'BTC':
+            counter_currency = Price.USDT
+        else:
+            counter_currency = Price.BTC
+        # midpoint_price must not be empty
         queryset = self.model.objects.exclude(midpoint_price__isnull=True
-        ).filter(transaction_currency=transaction_currency, resample_period=short_period)
+        ).filter(transaction_currency=transaction_currency, counter_currency=counter_currency, \
+                    resample_period=SHORT_PERIOD)
 
         return queryset.order_by('-timestamp')
--- settings/__init__.py
+++ settings/__init__.py
@@ -99,6 +99,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django.contrib.sites',
+    'django_filters',
 
     # PLUGINS
     'rest_framework',
@@ -245,6 +246,8 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+
+     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }
 
 
