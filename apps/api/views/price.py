c1c2daa6b793b34cf68fe579758b61dfee74b86e
Alex Boreysha <lexnex@gmail.com>
2018-03-30 09:38:59 +0200

    reduce number of workers, rest api price order by id

--- apps/api/views/price.py
+++ apps/api/views/price.py
@@ -45,7 +45,8 @@ class ListPrices(ListAPIView):
     model = serializer_class.Meta.model
     
     def get_queryset(self):
-        queryset = self.model.objects.order_by('-timestamp')
+        #queryset = self.model.objects.order_by('-timestamp')
+        queryset = self.model.objects.order_by('-id')
         queryset = filter_queryset_by_timestamp(self, queryset)
         return queryset
 
