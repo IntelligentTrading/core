9a464f1a9078092a2242aebe8ba2ec0d1686fa84
Alex Y <aalond@gmail.comconda info --envsgit config --global user.email aalond@gmail.com>
2018-05-18 19:12:59 +0300

    fix 3

--- taskapp/celery.py
+++ taskapp/celery.py
@@ -55,6 +55,13 @@ def setup_periodic_tasks(sender, **kwargs):
         name='at the beginning of every hour',
         )
 
+    sender.add_periodic_task(
+        crontab(minute=20, hour=16),
+        tasks.compute_and_save_indicators_for_all_sources.s(resample_period=SHORT),
+        name='at the beginning of every hour',
+        )
+
+
     # calculate MEDIUM period at the start of every 4 hours
     sender.add_periodic_task(
         crontab(minute=0, hour='*/4'),
