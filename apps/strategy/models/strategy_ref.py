aa1cb8b097c34a0ca02e454aa32a466a08aa17ef
Alex Y <aalond@gmail.comconda info --envsgit config --global user.email aalond@gmail.com>
2018-05-21 17:49:10 +0300

    remove strategy ref DB - 1

--- apps/strategy/models/strategy_ref.py
+++ apps/strategy/models/strategy_ref.py
@@ -1,21 +1,39 @@
 import logging
 from django.db import models
-from collections import namedtuple
+
 
 logger = logging.getLogger(__name__)
 
+# A datastructure to keep all possible strategies in the system
+from collections import namedtuple
+SignalRefType = namedtuple(
+    'SignalRefType',
+    'name implementation_module_name implementation_class_name description generated ')
 
+ALL_STRATEGIES = {
+    'rsi_simple' : SignalRefType(
+        name='RSI Simple',
+        implementation_module_name='apps.strategy.models.rsi_sma_strategies',
+        implementation_class_name='RsiSimpleStrategy',
+        description='Buy at RSI < 25, sell at RSI > 80',
+        generated = 'manual'
+    ),
+    'ichi_kumo_breakout' : SignalRefType(
+        name='Ichimoku Kumo Breakout',
+        implementation_module_name='apps.strategy.models.ichi_strategies',
+        implementation_class_name='IchiKumoBreakoutStrategy',
+        description='Buy at Ichimoku Kumo breakout up and sell at Ichimoku bearkout down',
+        generated='manual'
+    ),
+    'ann_simple' : SignalRefType(
+        name='ANN Simple',
+        implementation_module_name='apps.strategy.models.ai_strategies',
+        implementation_class_name='AnnSimpleStrategy',
+        description='Buy if ANN predict price will grow  and sell if it predicts that price goes down',
+        generated='manual'
+    )
 
-# SignalRefType = namedtuple(
-#     'SignalRefType',
-#     'name implementation_module_name implementation_class_name description generated ')
-#
-# ALL_STRATEGIES = {
-#
-#     'rsi_buy_1' : SignalRefType('RSI', 1, 1),
-#     'rsi_buy_2' : SignalRefType('RSI', 1, 2),
-#
-# }
+}
 
 
 class StrategyRef(models.Model):
@@ -32,9 +50,8 @@ class StrategyRef(models.Model):
     generated = models.CharField(max_length=16, null=False, blank=False) # manual/auto
     last_backtested_performance = models.FloatField(null=True)
 
-
-
 # get all class names
+# TODO: get it from ALL_STRATEGIES by copy it in a new object, then loop over ALL_STRATEGIES.popitem()
 def get_all_strategy_classes():
     '''
     :return: all strategies in the system as class objects
@@ -52,6 +69,26 @@ def get_all_strategy_classes():
         strategy_object_list.append(class_obj)
 
     return strategy_object_list
+
+# # get all class names
+# def get_all_strategy_classes():
+#     '''
+#     :return: all strategies in the system as class objects
+#     '''
+#     import importlib
+#     strategy_object_list = []
+#     strategy_string_list = list(StrategyRef.objects.all().values('implementation_module_name','implementation_class_name'))
+#
+#     for strategy in strategy_string_list:
+#         module_name = strategy['implementation_module_name']
+#         class_name = strategy['implementation_class_name']
+#         module = importlib.import_module(module_name)
+#
+#         class_obj = getattr(module, class_name)
+#         strategy_object_list.append(class_obj)
+#
+#     return strategy_object_list
+
 
 
 # pre-population of all strategies
@@ -85,3 +122,4 @@ def add_all_strategies():
     s3.save()
 
     return get_all_strategy_classes()
+
