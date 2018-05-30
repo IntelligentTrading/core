56d376b0dd0954e08dce987b21898c9c6a481920
Tom Counsell <other@tomcounsell.com>
2018-05-30 16:38:47 +0700

    optimize stats logic

--- apps/dashboard/views/main.py
+++ apps/dashboard/views/main.py
@@ -15,21 +15,20 @@ class Main(View):
 
     def get(self, request):
 
-        context = get_stats()
-
+        context = {
+            "signals": getSignalStats()
+        }
+        # print(context["signals"])
         return render(request, 'main.html', context)
 
 
-def get_stats():
+def getSignalStats():
     from apps.signal.models import Signal
     from datetime import datetime, timedelta
 
     stats = {}
 
     # ALL SIGNALS
-    stats["signals"] = {}
-    stats["signals"]["all"] = {}
-
     signals_24_hrs = Signal.objects.filter(
                 sent_at__isnull=False,
                 sent_at__gte=datetime.now()-timedelta(hours=24)
@@ -39,99 +38,53 @@ def get_stats():
                 sent_at__lte=datetime.now() - timedelta(hours=24)
             )
 
-    num_signals_24_hrs = signals_24_hrs.count()
-    stats["signals"]["all"]["num_signals_24_hrs"] = num_signals_24_hrs
 
-    num_signals_prev_24_hrs = signals_prev_24_hrs.count()
-    # stats["signals"]["all"]["num_signals_prev_24_hrs"] = num_signals_prev_24_hrs
+    # ADD FOR EACH SIGNAL TYPE
+    for signal_name in ["total", "RSI_Cumulative", "RSI", "SMA", "ANN_Simple",]:
 
-    num_signals_24_hrs_delta = num_signals_24_hrs - num_signals_prev_24_hrs
-
-    if num_signals_prev_24_hrs > 0:
-        num_signals_24_hrs_delta_percent = \
-            float(num_signals_24_hrs_delta) / num_signals_prev_24_hrs
-    else:
-        num_signals_24_hrs_delta_percent = 0.0
-    stats["signals"]["all"]["num_signals_24_hrs_delta_percent"] = \
-        num_signals_24_hrs_delta_percent
-
-
-    # HORIZONS FOR ALL SIGNALS
-    for horizon_tuple in HORIZON_CHOICES:
-        model_horizon_value, horizon_string = horizon_tuple
-
-        stats["signals"][horizon_string] = {}
-
-        horizon_signals_24_hrs = signals_24_hrs.filter(horizon=model_horizon_value)
-        horizon_signals_prev_24_hrs = signals_prev_24_hrs.filter(horizon=model_horizon_value)
-
-        num_horizon_signals_24_hrs = horizon_signals_24_hrs.count()
-        stats["signals"][horizon_string]["num_signals_24_hrs"] = num_horizon_signals_24_hrs
-
-        num_horizon_signals_prev_24_hrs = horizon_signals_prev_24_hrs.count()
-        # stats["signals"][horizon_string]["num_signals_prev_24_hrs"] = num_horizon_signals_prev_24_hrs
-
-        num_horizon_signals_24_hrs_delta = num_horizon_signals_24_hrs - num_horizon_signals_prev_24_hrs
-
-        if num_horizon_signals_prev_24_hrs > 0:
-            num_horizon_signals_24_hrs_delta_percent = \
-                float(num_horizon_signals_24_hrs_delta) / num_horizon_signals_prev_24_hrs
+        if signal_name is "total":
+            subset_signals_24_hrs = signals_24_hrs
+            subset_signals_prev_24_hrs = signals_prev_24_hrs
         else:
-            num_horizon_signals_24_hrs_delta_percent = 0.0
-            stats["signals"][horizon_string]["num_signals_24_hrs_delta_percent"] = \
-                num_horizon_signals_24_hrs_delta_percent
+            subset_signals_24_hrs = signals_24_hrs.filter(signal=signal_name)
+            subset_signals_prev_24_hrs = signals_prev_24_hrs.filter(signal=signal_name)
 
+        stats[signal_name] = {}
+        stats[signal_name]["all"] = {}
+        stats[signal_name]["all"]["num_signals_24_hrs"] = subset_signals_24_hrs.count()
+        stats[signal_name]["all"]["num_signals_prev_24_hrs"] = subset_signals_prev_24_hrs.count()
 
-    # RSI SIGNALS
-    stats["rsi_signals"] = {}
-    stats["rsi_signals"]["all"] = {}
-
-    rsi_signals_24_hrs = signals_24_hrs.filter(signal="RSI")
-    rsi_signals_prev_24_hrs = signals_prev_24_hrs.filter(signal="RSI")
-
-    num_signals_24_hrs = rsi_signals_24_hrs.count()
-    stats["rsi_signals"]["all"]["num_signals_24_hrs"] = num_signals_24_hrs
-
-    num_signals_prev_24_hrs = rsi_signals_prev_24_hrs.count()
-    # stats["signals"]["all"]["num_signals_prev_24_hrs"] = num_signals_prev_24_hrs
-
-    num_signals_24_hrs_delta = num_signals_24_hrs - num_signals_prev_24_hrs
-
-    if num_signals_prev_24_hrs > 0:
-        num_signals_24_hrs_delta_percent = \
-            float(num_signals_24_hrs_delta) / num_signals_prev_24_hrs
-    else:
-        num_signals_24_hrs_delta_percent = 0.0
-    stats["rsi_signals"]["all"]["num_signals_24_hrs_delta_percent"] = \
-        num_signals_24_hrs_delta_percent
-
-
-    # HORIZONS FOR RSI SIGNALS
-    for horizon_tuple in HORIZON_CHOICES:
-        model_horizon_value, horizon_string = horizon_tuple
-
-        stats["rsi_signals"][horizon_string] = {}
-
-        horizon_signals_24_hrs = rsi_signals_24_hrs.filter(horizon=model_horizon_value)
-        horizon_signals_prev_24_hrs = rsi_signals_prev_24_hrs.filter(horizon=model_horizon_value)
-
-        num_horizon_signals_24_hrs = horizon_signals_24_hrs.count()
-        stats["rsi_signals"][horizon_string]["num_signals_24_hrs"] = num_horizon_signals_24_hrs
-
-        num_horizon_signals_prev_24_hrs = horizon_signals_prev_24_hrs.count()
-        # stats["signals"][horizon_string]["num_signals_prev_24_hrs"] = num_horizon_signals_prev_24_hrs
-
-        num_horizon_signals_24_hrs_delta = num_horizon_signals_24_hrs - num_horizon_signals_prev_24_hrs
-
-        if num_horizon_signals_prev_24_hrs > 0:
-            num_horizon_signals_24_hrs_delta_percent = \
-                float(num_horizon_signals_24_hrs_delta) / num_horizon_signals_prev_24_hrs
+        num_signals_24_hrs_delta = stats[signal_name]["all"]["num_signals_24_hrs"] - stats[signal_name]["all"]["num_signals_prev_24_hrs"]
+        if stats[signal_name]["all"]["num_signals_prev_24_hrs"] > 0:
+            num_signals_24_hrs_delta_percent = \
+                float(num_signals_24_hrs_delta) / stats[signal_name]["all"]["num_signals_prev_24_hrs"]
         else:
-            num_horizon_signals_24_hrs_delta_percent = 0.0
-            stats["rsi_signals"][horizon_string]["num_signals_24_hrs_delta_percent"] = \
-                num_horizon_signals_24_hrs_delta_percent
+            num_signals_24_hrs_delta_percent = 0.0
+        stats[signal_name]["all"]["num_signals_24_hrs_delta_percent"] = num_signals_24_hrs_delta_percent
 
+        # ADD HORIZONS
+        for horizon_tuple in HORIZON_CHOICES:
+            model_horizon_value, horizon_string = horizon_tuple
 
-    print(stats)
+            stats[signal_name][horizon_string] = {}
+
+            horizon_signals_24_hrs = subset_signals_24_hrs.filter(horizon=model_horizon_value)
+            horizon_signals_prev_24_hrs = subset_signals_prev_24_hrs.filter(horizon=model_horizon_value)
+
+            num_horizon_signals_24_hrs = horizon_signals_24_hrs.count()
+            stats[signal_name][horizon_string]["num_signals_24_hrs"] = num_horizon_signals_24_hrs
+
+            num_horizon_signals_prev_24_hrs = horizon_signals_prev_24_hrs.count()
+            # stats["signals"][horizon_string]["num_signals_prev_24_hrs"] = num_horizon_signals_prev_24_hrs
+
+            num_horizon_signals_24_hrs_delta = num_horizon_signals_24_hrs - num_horizon_signals_prev_24_hrs
+
+            if num_horizon_signals_prev_24_hrs > 0:
+                num_horizon_signals_24_hrs_delta_percent = \
+                    float(num_horizon_signals_24_hrs_delta) / num_horizon_signals_prev_24_hrs
+            else:
+                num_horizon_signals_24_hrs_delta_percent = 0.0
+                stats[signal_name][horizon_string]["num_signals_24_hrs_delta_percent"] = \
+                    num_horizon_signals_24_hrs_delta_percent
 
     return stats
