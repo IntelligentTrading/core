4954bbbfb56825ffb4e1c6e7f2ee5bf2982dbe70
Alex Borey <lexnex@gmail.com>
2018-06-05 16:57:20 +0200

    [api] add required parameters to price api

--- apps/api/views/price.py
+++ apps/api/views/price.py
@@ -1,3 +1,5 @@
+from rest_framework import exceptions
+
 from rest_framework.generics import ListAPIView
 
 from apps.api.serializers import PriceSerializer
@@ -5,8 +7,6 @@ from apps.api.permissions import RestAPIPermission
 from apps.api.paginations import StandardResultsSetPagination, OneRecordPagination
 
 from apps.api.helpers import filter_queryset_by_timestamp, queryset_for_list_without_resample_period
-
-from apps.indicator.models import Price
 
 
 
@@ -19,17 +19,19 @@ class ListPrices(ListAPIView):
 
     For filtering
 
-        transaction_currency: -- string 'BTC', 'ETH' etc
-        counter_currency -- number 0=BTC, 1=ETH, 2=USDT, 3=XMR
-        source -- number 0=poloniex, 1=bittrex, 2=binance
+        transaction_currency: -- string 'BTC', 'ETH' etc (required)
+        counter_currency -- number 0=BTC, 1=ETH, 2=USDT, 3=XMR (required)
+        source -- number 0=poloniex, 1=bittrex, 2=binance (required)
         startdate -- from this date (inclusive). Example 2018-02-12T09:09:15
         enddate -- to this date (inclusive)
 
     For pagination
+
         cursor - indicator that the client may use to page through the result set
         page_size -- a numeric value indicating the page size
 
     Examples
+
         /api/v2/prices/?startdate=2018-01-26T10:24:37&enddate=2018-01-26T10:59:02
         /api/v2/prices/?transaction_currency=ETH&counter_currency=0
     """
@@ -41,16 +43,22 @@ class ListPrices(ListAPIView):
     filter_fields = ('source', 'transaction_currency', 'counter_currency')
 
     model = serializer_class.Meta.model
-    
+
     def get_queryset(self):
+        # check required parameters
+        for param in ('source', 'transaction_currency', 'counter_currency'):
+            if param not in self.request.query_params:
+                raise exceptions.NotFound(detail=f"Missing required parameter: {param}")
+
         queryset = filter_queryset_by_timestamp(self, self.model.objects)
         return queryset
 
 
 class ListPrice(ListAPIView):
-    """Return list of prices from Price model for {transaction_currency} with default counter_currency. 
+    """
+    Return list of prices from Price model for {transaction_currency} with default counter_currency.
     Default counter_currency is BTC. For BTC itself, counter_currency is USDT.
-    
+
     /api/v2/prices/{transaction_currency}
 
     URL query parameters
