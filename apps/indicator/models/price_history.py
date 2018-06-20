e1af8e583c63480d86ec0e80fbee966f3223cd5c
Alex Borey <lexnex@gmail.com>
2018-06-20 18:48:33 +0200

    add PriceHistory model

---
+++ apps/indicator/models/price_history.py
@@ -0,0 +1,29 @@
+#from datetime import timedelta, datetime
+import logging
+import pandas as pd
+
+from django.db import models
+
+from settings import SOURCE_CHOICES, COUNTER_CURRENCY_CHOICES
+
+
+
+logger = logging.getLogger(__name__)
+
+class PriceHistory(models.Model):
+    source = models.SmallIntegerField(choices=SOURCE_CHOICES, null=False)
+    transaction_currency = models.CharField(max_length=6, null=False)
+    counter_currency = models.SmallIntegerField(choices=COUNTER_CURRENCY_CHOICES, null=False)
+    open_p = models.BigIntegerField(null=True) # open_p because open is built-in name in Python
+    high = models.BigIntegerField(null=True)
+    low = models.BigIntegerField(null=True)
+    close = models.BigIntegerField(null=True) # please use this as price
+
+    timestamp = models.DateTimeField(null=False) # store UTC, timestamp=datetime.datetime.utcfromtimestamp(float(timestamp)/1000.0)
+
+
+    # INDEX FIXME enable it later after importing all data (it's faster w/o index)
+    # class Meta:
+    #     indexes = [
+    #         models.Index(fields=['timestamp', 'source', 'transaction_currency', 'counter_currency']),
+    #     ]
