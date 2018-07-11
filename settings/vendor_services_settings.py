ee2a73d2d5ce5cbb426dcad006d93e54a8c3c765
Alex Borey <lexnex@gmail.com>
2018-07-11 13:42:40 +0200

    switch from mysql to postgresql

--- settings/vendor_services_settings.py
+++ settings/vendor_services_settings.py
@@ -40,7 +40,7 @@ if not LOCAL:
 
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
