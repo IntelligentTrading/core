cbcecf0557c9cb0c1cee1469bd01e6e1e08de48a
Alex Borey <lexnex@gmail.com>
2018-07-14 12:30:06 +0200

    Number of workers from 5 to 4 to reduce memory consumption. Temporary disable VBI

--- settings/__init__.py
+++ settings/__init__.py
@@ -280,7 +280,8 @@ EMIT_SMA = True
 EMIT_RSI = True
 #RUN_ANN = True
 RUN_ANN = False
-RUN_BEN = True
+#RUN_BEN = True
+RUN_BEN = False
 MODIFY_DB = True
 
 EMIT_SIGNALS = os.environ.get("EMIT_SIGNALS", "true").lower() == "true" # emit if no variable set or when it set to 'true', env variables are strings
