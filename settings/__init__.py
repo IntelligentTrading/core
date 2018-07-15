57f44e7aabc8d73268a4feed5ab5e428bf31ffa1
Alex Borey <lexnex@gmail.com>
2018-07-15 10:51:23 +0200

    enable vbi

--- settings/__init__.py
+++ settings/__init__.py
@@ -279,8 +279,7 @@ time_speed = 1  # set to 1 for production, 10 for fast debugging
 EMIT_SMA = True
 EMIT_RSI = True
 RUN_ANN = True
-#RUN_BEN = True
-RUN_BEN = False
+RUN_BEN = True
 MODIFY_DB = True
 
 EMIT_SIGNALS = os.environ.get("EMIT_SIGNALS", "true").lower() == "true" # emit if no variable set or when it set to 'true', env variables are strings
