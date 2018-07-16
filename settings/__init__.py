ad098fe2ea7d39bfba30c82af979544240fdef86
Alex Borey <lexnex@gmail.com>
2018-07-16 12:37:43 +0200

    set loglevel DEBUG for production

--- settings/__init__.py
+++ settings/__init__.py
@@ -42,7 +42,8 @@ else:
 if LOCAL:
     log_level = logging.DEBUG
 elif PRODUCTION:
-    log_level = logging.INFO
+#    log_level = logging.INFO
+    log_level = logging.DEBUG
 else:
     log_level = logging.DEBUG
 
