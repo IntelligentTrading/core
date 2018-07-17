ec8a0b962cb7c1e221bdf6e0b4015fcd9383053b
Tom Counsell <other@tomcounsell.com>
2018-07-17 19:31:39 +0700

    refactor into abstract class for inheritance

--- TA/storages/abstract_timeseries_storage.py
+++ TA/storages/abstract_timeseries_storage.py
@@ -1,3 +1,60 @@
+import logging
+from abc import ABC
+
+
+class IndicatorException(Exception):
+    pass
+
+
+class AbstractIndicator(ABC):
+    """
+    stores prices in a sorted set unique to each ticker and exchange
+    redis keys
+    todo: split the db by each exchange source
+    todo: refactor to add short, medium, long (see resample_period in abstract_indicator)
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.ticker = kwargs['ticker']  # str eg. BTC_USD
+        self.exchange = kwargs['exchange']  # str or int
+
+        self.unix_timestamp = int(kwargs.get('timestamp'))  # int eg. 1483228800
+        if self.unix_timestamp % 300 != 0 or self.unix_timestamp < 1483228800:
+            raise IndicatorException("fix your timestamp. should be % 300 and > 1483228800 (2017-01-01)")
+        # self.resample_period = 300  # 5 min
+        self.db_key_suffix = ""
+
+        self.force_save = kwargs.get('force_save', False)
+
+
+    def __str__(self):
+        return str(self.get_db_key())
+
+
+    def get_db_key(self):
+        if not self.force_save:
+            # validate some rules here?
+            pass
+        return "{ticker}:{exchange}:{class_name}:{timestamp}".format(
+            ticker=self.ticker, exchange=self.exchange,
+            class_name=str(self.__class__.__name__+self.db_key_suffix),
+            timestamp=self.unix_timestamp)
+
+
+    def save(self, pipeline=False):
+        # example >>> redis.zadd('my-key', 'name1', 1.1)
+        if pipeline:
+            pipeline.zadd(self.get_db_key(), self.value, int(self.unix_timestamp))
+            return pipeline
+        else:
+            from TA.app import db
+            return db.zadd(self.get_db_key(), self.value, int(self.unix_timestamp))
+
+
+    class Meta:
+        abstract = True
+
+
 """
 We can scan the newest or oldest event ids with ZRANGE 4,
 maybe later pulling the events themselves for analysis.
--- TA/storages/price.py
+++ TA/storages/price.py
@@ -1,6 +1,7 @@
 from abc import ABC
 import logging
 
+from TA.storages.abstract_timeseries_storage import AbstractIndicator
 
 price_indexes = [
     "open_price", "close_price", "low_price", "high_price",
@@ -15,50 +16,28 @@ volume_indexes = [
 class PriceException(Exception):
     pass
 
-class Price(ABC):
-    """
-    stores prices in a sorted set unique to each ticker and exchange
-    redis keys
-    todo: split the db by each exchange source
-    todo: refactor to add short, medium, long (see resample_period in abstract_indicator)
-    """
+class Price(AbstractIndicator):
 
     def __init__(self, *args, **kwargs):
-        self.ticker = kwargs['ticker']
-        self.exchange = kwargs['exchange']
+        super().__init__()
         self.index = kwargs.get('index', "close")
-        self.force_save = kwargs.get('force_save', False)
-
         self.value = kwargs.get('value')
-        self.unix_timestamp = int(kwargs.get('timestamp'))
 
-        if self.unix_timestamp % 300 != 0 or self.unix_timestamp < 1483228800:
-            raise PriceException("fix your timestamp. should be % 300 and > 1483228800 (2017-01-01)")
 
-    def __str__(self):
-        return str(self.key)
+    def save(self, pipeline):
 
-    @property
-    def key(self):
-        if not self.force_save:
-            if not self.index in price_indexes:
-                raise PriceException("unknown index")
-        return "{ticker}:{exchange}:{index}:{timestamp}".format(
-            ticker=self.ticker, exchange=self.exchange,
-            index=self.index, timestamp=self.unix_timestamp)
-
-    def save(self, pipeline=False):
         # meets basic requirements for saving
         if not all(self.ticker, self.exchange,
                    self.index, self.value,
                    self.unix_timestamp):
-            logging.critical("incomplete information, cannot save \n" + str(self.__dict__))
+            logging.error("incomplete information, cannot save \n" + str(self.__dict__))
             raise PriceException("save error, missing data")
 
-        # example >>> redis.zadd('my-key', 'name1', 1.1)
-        if pipeline:
-            pipeline.zadd(self.key, self.value, int(self.unix_timestamp))
-            return pipeline
-        else:
-            from TA.app import db
-            return db.zadd(self.key, self.value, int(self.unix_timestamp))
+        if not self.force_save:
+            if not self.index in price_indexes:
+                logging.error("price index not in approved list, raising exception...")
+                raise PriceException("unknown index")
+
+        self.db_key_suffix = ":{index}".format(self.index)
+        super().save(pipeline=pipeline)
+
