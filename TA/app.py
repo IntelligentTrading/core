bb591c297fe445557512ccbce99faf07d674880b
Tom Counsell <other@tomcounsell.com>
2018-07-17 15:31:07 +0700

    add logging

--- TA/app.py
+++ TA/app.py
@@ -1,16 +1,24 @@
 from flask import Flask, logging
-from flask_restful import Resource, Api
+from flask_restful import reqparse, abort, Api, Resource
 import redis
 
 
 REDIS_HOST, REDIS_PORT = "127.0.0.1:6379".split(":")
 pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, db=0)
 db = redis.Redis(connection_pool=pool)
+logging.info("Redis connection established.")
+
 
 app = Flask(__name__)
 api = Api(app)
+logging.info("Flask app instantiated.")
 
 
+# ROUTING
 from TA.resources.price import PriceAPI
-api.add_resource(PriceAPI, '/price')
+api.add_resource(PriceAPI, '/price/<ticker>')
 
+
+
+
+logging.info("Flask resources and routes are ready.")
