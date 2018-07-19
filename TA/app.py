f24eab21b193f9ac868ad88763690f5ca2ee7360
Tom Counsell <other@tomcounsell.com>
2018-07-20 00:28:05 +0700

    fix logger

--- TA/app.py
+++ TA/app.py
@@ -1,24 +1,28 @@
-from flask import Flask, logging
+import logging
+from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 import redis
 
+logger = logging.getLogger('flask_api')
+logger.setLevel(logging.DEBUG)
 
 REDIS_HOST, REDIS_PORT = "127.0.0.1:6379".split(":")
 pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, db=0)
 db = redis.Redis(connection_pool=pool)
-logging.info("Redis connection established.")
+logger.info("Redis connection established.")
 
 
 app = Flask(__name__)
 api = Api(app)
-logging.info("Flask app instantiated.")
+logger.info("Flask app instantiated.")
 
 
 # ROUTING
 from TA.resources.price import PriceAPI
-api.add_resource(PriceAPI, '/price/<ticker>')
+api.add_resource(PriceAPI, '/api/price/<string:ticker>')
+
+from TA.resources.resampled import PriceVolumeResampledAPI
+api.add_resource(PriceVolumeResampledAPI, '/api/resampled/<string:ticker>')
 
 
-
-
-logging.info("Flask resources and routes are ready.")
+logger.info("Flask resources and routes are ready.")
