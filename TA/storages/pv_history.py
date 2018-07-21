4785a483b12d5860ea2db32b339d80440d003053
Tom Counsell <other@tomcounsell.com>
2018-07-21 15:45:01 +0700

    storage for price and volume

--- TA/storages/pv_history.py
+++ TA/storages/pv_history.py
@@ -2,15 +2,23 @@ from abc import ABC
 import logging
 
 from TA.app import TAException
-from TA.storages.indicator import TimeseriesIndicator
-from TA.storages.pv_history import price_indexes
+from TA.storages.timeseries_storage import TimeseriesStorage
+
+price_indexes = [
+    "open_price", "close_price", "low_price", "high_price",
+    "midpoint_price", "mean_price", "price_variance",
+]
+
+volume_indexes = [
+    "open_volume", "close_volume", "low_volume", "high_volume",
+]
 
 
-class PriceException(TAException):
+class PriceVolumeHistoryException(TAException):
     pass
 
 
-class PriceStorage(TimeseriesIndicator):
+class PriceVolumeHistoryStorage(TimeseriesStorage):
 
     def __init__(self, *args, **kwargs):
         super().__init__()
@@ -25,17 +33,17 @@ class PriceStorage(TimeseriesIndicator):
                    self.index, self.value,
                    self.unix_timestamp):
             logging.error("incomplete information, cannot save \n" + str(self.__dict__))
-            raise PriceException("save error, missing data")
+            raise PriceVolumeHistoryException("save error, missing data")
 
         if not self.force_save:
             if not self.index in price_indexes:
                 logging.error("price index not in approved list, raising exception...")
-                raise PriceException("unknown index")
+                raise PriceVolumeHistoryException("unknown index")
 
         self.db_key_suffix = ":{index}".format(self.index)
         super().save(pipeline=pipeline)
 
 
-class BlockchainStatsHistory(TimeseriesIndicator):
+class BlockchainStatsHistory(TimeseriesStorage):
     def __init__(self, *args, **kwargs):
         super().__init__()
