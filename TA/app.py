d664c97b5aab3d16910fa4932f8aebf92b0269ed
Tom Counsell <other@tomcounsell.com>
2018-07-21 14:22:16 +0700

    env vars updates

--- TA/app.py
+++ TA/app.py
@@ -1,3 +1,4 @@
+import os
 import logging
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
@@ -5,6 +6,7 @@ import redis
 
 logger = logging.getLogger('flask_api')
 logger.setLevel(logging.DEBUG)
+
 
 class TAException(Exception):
     def __init__(self, message):
@@ -18,15 +20,24 @@ class SuchWowException(TAException):
         logger.debug(f'\n\n{such_wow}\n\n{message}\n\n{such_wow}')
 
 
+SIMULATED_ENV = os.get("env", "TEMP")
+# todo: use this to mark keys in redis db, so they can be separated and deleted
 REDIS_HOST, REDIS_PORT = "127.0.0.1:6379".split(":")
 pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, db=0)
-db = redis.Redis(connection_pool=pool)
+# todo: change db=1,2,3 for stage/prod/test envs?
+database = redis.Redis(connection_pool=pool)
 logger.info("Redis connection established.")
 
 
 app = Flask(__name__)
 api = Api(app)
 logger.info("Flask app instantiated.")
+
+# if env == "PRODUCTION":
+#     run scheduler for cleanup of
+
+
+
 
 
 # ROUTING
