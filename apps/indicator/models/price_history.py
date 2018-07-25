80773f31259fe3d935b50410023ade9c8dbbc84a
Alex Y <aalond@gmail.comconda info --envsgit config --global user.email aalond@gmail.com>
2018-07-25 16:53:25 +0300

    final elimination of Price usage

--- apps/indicator/models/price_history.py
+++ apps/indicator/models/price_history.py
@@ -40,3 +40,58 @@ class PriceHistory(models.Model):
             models.Index(fields=['timestamp', 'transaction_currency', 'counter_currency', 'source']),
         ]
         unique_together = ('timestamp', 'transaction_currency', 'counter_currency', 'source')
+
+
+# get n last price records
+def get_n_last_prices_ts(n, source, transaction_currency, counter_currency ):
+    back_in_time_records = list(PriceHistory.objects.filter(
+        source=source,
+        transaction_currency=transaction_currency,
+        counter_currency=counter_currency,
+        timestamp__gte=datetime.now() - timedelta(minutes=n)
+    ).values('timestamp', 'price').order_by('timestamp'))
+
+    if back_in_time_records:
+        return pd.Series(
+            data= [rec['price'] for rec in back_in_time_records],
+            index = [rec['timestamp'] for rec in back_in_time_records]
+        )
+
+
+def get_currency_value_from_string(currency_string):
+    currency_dict = {str: i for (i, str) in COUNTER_CURRENCY_CHOICES}
+    return currency_dict.get(currency_string, None)
+
+
+def int_price2float(int_price):
+    float_price = float(int_price * 10**-8)
+
+
+def get_price_at_timepoint(timestamp, source, transaction_currency, counter_currency, resample_period)->int:
+    prices_range = list(PriceHistory.objects.filter(
+        source=source,
+        transaction_currency=transaction_currency,
+        counter_currency=counter_currency,
+        timestamp__gte = timestamp - timedelta(minutes=10),  # 10 min ahead in time
+        timestamp__lte=timestamp + timedelta(minutes=10),  # 10 min back in time
+    ).values('timestamp',  'price').order_by('timestamp'))
+
+    #convert to a timeseries
+    if prices_range:
+        ts = [rec['timestamp'] for rec in prices_range]
+        close_prices_ts = pd.Series(data=[rec['price'] for rec in prices_range], index=ts)
+    else:
+        logger.error(' we dont have any price in 10 min proximity of the date you provided:  ' + str(timestamp) + ' :backtesting is not possible')
+        return None
+
+    # check if we have a price at a given timestamp and if not we interpolate
+    if timestamp in close_prices_ts.index:
+        price = close_prices_ts[timestamp]
+    else:
+        # add our missing index, resort and then interpolate
+        close_prices_ts = close_prices_ts.append(pd.Series(None, index=[timestamp]))
+        close_prices_ts.sort_index(inplace=True)
+        close_prices_ts = close_prices_ts.interpolate(method='linear', limit=10, limit_direction='both')
+        price = int(close_prices_ts[timestamp])
+
+    return price
