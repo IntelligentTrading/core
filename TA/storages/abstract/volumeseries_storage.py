dc57ab39feacaa142d01ab26c4ec0ef3eb92c814
Tom Counsell <other@tomcounsell.com>
2018-07-26 20:12:04 +0700

    room for a different kind of series 😉

---
+++ TA/storages/abstract/volumeseries_storage.py
@@ -0,0 +1,19 @@
+from TA.app import logger, TAException, database, set_of_known_sets_in_redis
+from TA.storages.abstract.key_value import KeyValueStorage
+
+
+class StorageException(TAException):
+    pass
+
+class VolumeseriesException(TAException):
+    pass
+
+
+class VolumeseriesStorage(KeyValueStorage):
+    """
+    stores things in a sorted set unique to each ticker and exchange
+    ordered by blocks of volume as opposed to time
+    todo: split the db by each exchange source
+    todo: refactor to add short, medium, long (see resample_period in abstract_indicator)
+    """
+    describer_class = "volumeseries"
