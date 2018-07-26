f5dca51d2ac305a45d02ffb9b563bfe667d1a160
Tom Counsell <other@tomcounsell.com>
2018-07-26 20:15:55 +0700

    start subscriber class

---
+++ TA/storages/data/blockchain.py
@@ -0,0 +1,11 @@
+from TA.app import TAException, logger
+from TA.storages.abstract.indicator import IndicatorStorage
+
+
+class BlockchainHistoryException(TAException):
+    pass
+
+
+class BlockchainHistory(IndicatorStorage):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
--- TA/storages/data/price.py
+++ TA/storages/data/price.py
@@ -1,12 +1,33 @@
 import logging
+from abc import ABC
 
-from TA.app import TAException
+from TA.app import TAException, database
 from TA.storages.abstract.indicator import IndicatorStorage
 from TA.storages.data.pv_history import price_indexes
 
 
 class PriceException(TAException):
     pass
+
+
+class DataSubscriber(ABC):
+
+    def __init__(self):
+        from TA.app import database
+        self.pubsub = database.pubsub()
+
+        from TA.storages.data.pv_history import PriceVolumeHistoryStorage
+        self.pubsub.subscribe(PriceVolumeHistoryStorage.describer_class)
+
+    # def go_go_go(self):
+    #     data_event = self.pubsub.get_message()
+    #
+    #     timestamp = parse_out_timestamp(data_event['message'])
+    #     # close to a five minute period mark? (+ or - 45 seconds)
+    #     seconds_from_five_min = int(history_instance.timestamp)+45 % 300
+    #     if seconds_from_five_min < 90:
+    #         # we are close to a 5min marker, notify subscribers
+    #         do something
 
 
 class PriceStorage(IndicatorStorage):
@@ -33,8 +54,3 @@ class PriceStorage(IndicatorStorage):
 
         self.db_key_suffix = ":{index}".format(self.index)
         return super().save(pipeline=pipeline)
-
-
-class BlockchainStatsHistory(IndicatorStorage):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
