0ace4f4cdad975f0461b3179ad5cfe05f9ef7779
Tom Counsell <other@tomcounsell.com>
2018-08-10 19:35:45 +0700

    super simple memory cleaner

--- apps/TA/storages/data/memory_cleaner.py
+++ apps/TA/storages/data/memory_cleaner.py
@@ -1,36 +1,41 @@
 import logging
+import time
 
-from apps.TA import TAException
-from apps.TA.storages.abstract.subscriber import TickerSubscriber
-from apps.TA.storages.data.pv_history import PriceVolumeHistoryStorage, all_indexes
 from settings.redis_db import database
 
 logger = logging.getLogger(__name__)
 
 
-class CleanerException(TAException):
-    pass
+def redisCleanup():
+
+    do_not_disturb = bool(int(database.get("working on old stuff").decode("utf-8")))
+    if do_not_disturb:
+        return
 
 
-class CleanerSubscriber(TickerSubscriber):
+    now_timestamp = int(time.time())
 
-    classes_subscribing_to = [
-        PriceVolumeHistoryStorage
-    ]
+    # PriceVolumeHistoryStorage
+    # delete all values 2 hours old or older
+    old_for_pv_history = now_timestamp - (3600*2)
 
-    def handle(self, channel, data, *args, **kwargs):
+    pv_history_keys = database.keys(f'*:PriceVolumeHistoryStorage:*')
+    for pv_history_key in pv_history_keys:
+        try:
+            database.zremrangebyscore(pv_history_key, 0, old_for_pv_history)
+        except Exception as e:
+            logger.error(str(e))
 
-        if channel == PriceVolumeHistoryStorage.__name__:
-            try:
-                # parse from data["key"] which should come like
-                # f'{data_history.ticker}:{data_history.exchange}:{data_history.timestamp}'
-                [ticker, exchange, timestamp] = data["key"].split(":")
 
-            except Exception as e:
-                raise CleanerException("PriceVolumeHistoryStorage key not recognized. " + str(e))
+    # PriceStorage
+    # delete all values 200 days old or older
+    old_for_price_history = now_timestamp - (3600*24*200)
 
-            else:
+    price_history_keys = database.keys(f'*:PriceStorage:*')
+    for price_history_key in price_history_keys:
+        try:
+            database.zremrangebyscore(price_history_key, 0, old_for_price_history)
+        except Exception as e:
+            logger.error(str(e))
 
-                for index in all_indexes:
-                    sorted_set_key = f'{ticker}:{exchange}:PriceVolumeHistoryStorage:{index}'
-                    database.zremrangebyscore(sorted_set_key, 0, int(timestamp)-(60*90))  # 90 minutes or older
+
