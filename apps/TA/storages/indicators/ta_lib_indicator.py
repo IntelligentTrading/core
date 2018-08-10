f21dadb9af9ce9c0f63a755e4521125fe48485a2
Tom Counsell <other@tomcounsell.com>
2018-08-10 13:09:43 +0700

    SMA indicator

--- apps/TA/storages/indicators/ta_lib_indicator.py
+++ apps/TA/storages/indicators/ta_lib_indicator.py
@@ -1,49 +1,67 @@
-from abc import ABC
-
 import talib
-
 import numpy as np
 
-# note that all ndarrays must be the same length!
-from apps.TA.storages.abstract.ticker import TickerStorage
-from apps.TA.storages.data.pv_history import PriceVolumeHistoryStorage
-from settings import logger
+from apps.TA import PERIODS_1HR, PERIODS_4HR, PERIODS_24HR, HORIZONS
+from apps.TA.storages.abstract.indicator import IndicatorStorage
+from apps.TA.storages.abstract.subscriber import TASubscriber
+from apps.TA.storages.data.price import PriceStorage
+from settings import logger, PERIODS_LIST
+
+# PERIODS_LIST = list([60,240,1440])
 
 SMA_LIST = [9, 20, 26, 30, 50, 52, 60, 120, 200]
-INDEXES = ['open', 'high', 'low', 'close', 'volume']
 
-class SMA(ABC):
+class SMAStorage(IndicatorStorage):
 
     def __init__(self, *args, **kwargs):
-        self.ticker = kwargs['ticker']  # str eg. BTC_USD
-        self.exchange = str(kwargs.get('exchange', ""))  # str or int
-        self.periods = kwargs.get('periods', 50)
-
-    def get_data(self):
-        data = {}
-        for index in INDEXES:
-            results_dict = PriceVolumeHistoryStorage.query(
-                ticker=self.ticker,
-                exchange=self.exchange,
-                index=index,
-                periods=self.periods
-            )
-            logger.debug(results_dict)
-            data['index'] = np.array(
-                [float(v) for v in results_dict['values']]
-            )
-        return data
-
-    def __call__(self, *args, **kwargs):
-        inputs = self.get_data()
-        return talib.SMA(inputs, timeperiod=self.periods)[-1]
+        super().__init__(*args, **kwargs)
 
 
+
+class SMASubscriber(TASubscriber):
+
+    classes_subscribing_to = [
+        PriceStorage
+    ]
+
+
+    def handle(self, channel, data, *args, **kwargs):
+        [ticker, exchange, class_describer, index] = data.split(":")
+        # eg. "BLZ_ETH:binance:PriceStorage:close_price"
+
+        if index != 'close_price':
+            return
+
+        #todo: this can be refactored into only one query!!
+        #todo: after one query for all values, then cut to sizes for horizons and periods
+        #todo: also there is some overlap. h=48, p=30 is the same as h=12,p=120
+
+        for horizon in HORIZONS:
+            for periods in SMA_LIST:
+
+                results_dict = PriceStorage.query(
+                    ticker=ticker,
+                    exchange=exchange,
+                    index=index,
+                    periods_key=None, # PriceStorage not unique to periods by key
+                    periods_range=periods*horizon
+                )
+                logger.debug(results_dict)
+                value_array = [float(v) for v in results_dict['values']]
+                if len(value_array) > periods*horizon:
+                    value_array = value_array[-periods*horizon:0]
+
+                value_np_array = np.array(value_array)
+
+                sma_value = talib.SMA(value_array, timeperiod=len(value_array))[-1]
+
+                new_sma_storage = SMAStorage(ticker=ticker,
+                                             exchange=exchange, timestamp=908)
 
 
 # sorted_set_key = "BTC_USDT:poloniex:PriceVolumeHistoryStorage:close_price"
 
-#
+# note that all ndarrays must be the same length!
 # inputs = {
 #     'open': np.random.random(100),
 #     'high': np.random.random(100),
