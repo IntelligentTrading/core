14aa411e3a41dfc44233b7b4bf0580ca76a0e11f
alexaibio <github.alex.ai@gmail.com>
2018-08-27 15:39:20 +0300

    add new ANN models to a DB

--- apps/ai/management/commands/add_nn_model.py
+++ apps/ai/management/commands/add_nn_model.py
@@ -12,13 +12,14 @@ class Command(BaseCommand):
     Run this command if you need to add a new NN model
     Keras or TF model must be already uploaded to S3
     '''
-    help = 'Add a new NN model to a database '
+    help = 'Add all NN models to the database '
 
     def handle(self, *args, **options):
         logger.info("Adding a new Neural Network model to a database")
 
         # check if it exists
-        # TODO: add automatic import from S3 + command line questionaaire about text fields + may be link to Notebook
+        # TODO: need to remove that later... it is an ugly way to initialize DB records
+        # TODO: change to dictionary of move to migration
 
         if not AnnModel.objects.filter(s3_model_file = 'lstm_model_2_2.h5').exists():
             AnnModel.objects.create(
@@ -43,4 +44,81 @@ class Command(BaseCommand):
                 )
             logger.info("Done.")
         else:
-            logger.info(" NN model already exists")
+            logger.info(" lstm_model_2_2.h5 model already exists")
+
+
+        if not AnnModel.objects.filter(s3_model_file = 'lstm_short_60m_160_8_3class_return_0.03.h5').exists():
+            AnnModel.objects.create(
+                timestamp=time.time(),
+                source = POLONIEX,
+                model_name = 'LSTM',
+                s3_model_file = 'lstm_short_60m_160_8_3class_return_0.03.h5',
+                s3_notebook_file = 'NA',
+                period = 60,  # min
+
+                slide_win_size = 160, # so timewise it is 200 x 10min = 33,3 hours
+                predicted_win_size = 8, # 90 x 10min = 15 hours
+                delta_tolerance = 0.03, # +/- 2%
+
+                train_accuracy = 0,
+                #train_f1_score = models.FloatField(null=True)
+                validation_accuracy = 0,
+                #validation_f1_score = models.FloatField(null=True)
+
+                train_data_description = "ETH XRP ETC DASH LTC ETH ETC OMG XRP XMR LTC BCH EOS XLM ADA TRX NEO XEM ZEC BNB VET",
+                features_description = '4 : price, volume, price variance, volume variance, F1: [0.645 0.232 0.325] ||  PRECISION: [0.884 0.161 0.238]  ||  RECALL: [0.507 0.41  0.513]'
+                )
+            logger.info("Done.")
+        else:
+            logger.info(" lstm_short_60m_160_8_3class_return_0.03.h5 model already exists")
+
+
+        if not AnnModel.objects.filter(s3_model_file = 'lstm_medium_240m_100_20_3class_return_0.1.h5').exists():
+            AnnModel.objects.create(
+                timestamp=time.time(),
+                source = POLONIEX,
+                model_name = 'LSTM',
+                s3_model_file = 'lstm_medium_240m_100_20_3class_return_0.1.h5',
+                s3_notebook_file = 'NA',
+                period = 240,  # min
+
+                slide_win_size = 100, # so timewise it is 200 x 10min = 33,3 hours
+                predicted_win_size = 20, # 90 x 10min = 15 hours
+                delta_tolerance = 0.1, # +/- 2%
+
+                train_accuracy = 0,
+                #train_f1_score = models.FloatField(null=True)
+                validation_accuracy = 0,
+                #validation_f1_score = models.FloatField(null=True)
+
+                train_data_description = "ETH XRP ETC DASH LTC ETH ETC OMG XRP XMR LTC BCH EOS XLM ADA TRX NEO XEM ZEC BNB VET",
+                features_description = ' '
+                )
+            logger.info("Done.")
+        else:
+            logger.info(" lstm_medium_240m_100_20_3class_return_0.1.h5 model already exists")
+
+        if not AnnModel.objects.filter(s3_model_file = 'lstm_long_1440m_28_10_class3_return_0.1.h5').exists():
+            AnnModel.objects.create(
+                timestamp=time.time(),
+                source = POLONIEX,
+                model_name = 'LSTM',
+                s3_model_file = 'lstm_long_1440m_28_10_class3_return_0.1.h5',
+                s3_notebook_file = 'NA',
+                period = 1440,  # min
+
+                slide_win_size = 28, # so timewise it is 200 x 10min = 33,3 hours
+                predicted_win_size = 10, # 90 x 10min = 15 hours
+                delta_tolerance = 0.1, # +/- 2%
+
+                train_accuracy = 0,
+                #train_f1_score = models.FloatField(null=True)
+                validation_accuracy = 0,
+                #validation_f1_score = models.FloatField(null=True)
+
+                train_data_description = "ETH XRP ETC DASH LTC ETH ETC OMG XRP XMR LTC BCH EOS XLM ADA TRX NEO XEM ZEC BNB VET",
+                features_description = ' '
+                )
+            logger.info("Done.")
+        else:
+            logger.info(" lstm_long_1440m_28_10_class3_return_0.1.h5 model already exists")
