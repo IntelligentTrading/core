09799e8a088f5fcf96034b4d8fc5ed2ddf7b63a3
Tom Counsell <other@tomcounsell.com>
2018-11-12 18:18:03 +0100

    add note

--- apps/api/permissions.py
+++ apps/api/permissions.py
@@ -5,6 +5,6 @@ class RestAPIPermission(permissions.BasePermission):
     message = 'Invalid or missing API Key.'
 
     def has_permission(self, request, view):
-        api_key = request.META.get('HTTP_API_KEY', '')
+        api_key = request.META.get('HTTP_API_KEY', '') # for some reason, in the headers, they paramter is actually "API-KEY"
         return api_key == REST_API_SECRET_KEY # Allow access if True
 
