5f0157323437c5d8b02a575ebe0b7320bd558bfe
sodogemuchwow <doge1337wow@gmail.com>
2018-11-16 16:33:27 +0100

    More cleanup, loading model and tokenizer from S3.

--- apps/sentiment/urls.py
+++ apps/sentiment/urls.py
@@ -5,4 +5,6 @@ app_name = 'sentiment'
 
 urlpatterns = [
     url(r'^$', SentimentDashboard.as_view(), name='sentiment'),
+    url(r'^(?P<timestamp>[\w\-]+)/$', SentimentDashboard.as_view(), name='sentiment_timestamp'),
+
 ]
